4c76924f7b1143190de0a5bfe77a2682ba0ad7ec
Gowtham Prashath <106882708+gowtham265254@users.noreply.github.com>
2023-02-18 13:01:25 +0530

    Add files via upload

---
+++ http-server/index.js
@@ -0,0 +1,56 @@
+// let args = process.argv;
+// const aa=args[3];
+// const cc=Number(aa);
+const readline = require("readline");
+const http=require('http');
+const fs=require('fs');
+const lineDetail = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+lineDetail.question(`node index.js --port `, (port) => {
+    
+    http.createServer(onRequest).listen(Number(port));
+  lineDetail.close();
+});
+
+
+function onRequest(request,response){
+    response.writeHead(200,{'content-type':'text/html'});
+    // if(request.url=='/'){
+    //     fs.readFile('./home.html',null,function(err,data){
+    //         if(err){
+    //             response.writeHead(404);
+    //             response.write('file not fount');
+    //         }else{
+    //             response.write(data);
+    //         }
+    //         response.end();
+    //     });
+    // }
+    if(request.url=='/'){
+        fs.readFile('./project.html',null,function(err,data){
+            if(err){
+                response.writeHead(404);
+                response.write('file not fount');
+            }else{
+                response.write(data);
+            }
+            response.end();
+        });
+    }
+    if(request.url=='/registration'){
+        fs.readFile('./registration.html',null,function(err,data){
+            if(err){
+                response.writeHead(404);
+                response.write('file not fount');
+            }else{
+                response.write(data);
+            }
+            response.end();
+        });
+    }
+}
+
+//
